4b9530f073bc8157849b99874570f4bd16bd3f79
mariblan <99274158+mariblan@users.noreply.github.com>
2023-01-20 13:37:58 +0100

    step2

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const app = express();
 const port = 4000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-    });
+  res.send(`
+    <h1>Adopt Pet!</h1>
+    <p>Browse through the links below to find your new furry friend:</p>
+    <ul>
+        <li><a href="/dogs">Dogs</a></li>
+        <li><a href="/cats">Cats</a></li>
+        <li><a href="/cats">Rabbits</a></li>
+    </ul>
+    `);
+});
 
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
-    });
+  console.log(`App listening at http://localhost:${port}`);
+});
