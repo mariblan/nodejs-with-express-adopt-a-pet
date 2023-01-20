87ca940cc32eb39d07220d2632985d89af73b741
mariblan <99274158+mariblan@users.noreply.github.com>
2023-01-20 16:41:06 +0100

    step5

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 4000;
-
+// const { animals }= require('./data/animals');
+// import { pets } from './helper.js';
+const pets = require('./helper.js');
 // With the string interpolation you can return more than one line of code
 // You can write HTML code in the string and it will be rendered in the browser
 app.get('/', (req, res) => {
@@ -11,15 +13,34 @@ app.get('/', (req, res) => {
     <ul>
         <li><a href="/animals/dogs">Dogs</a></li>
         <li><a href="/animals/cats">Cats</a></li>
-        <li><a href="/animals/cats">Rabbits</a></li>
+        <li><a href="/animals/rabbits">Rabbits</a></li>
     </ul>
     `);
 });
 
 app.get('/animals/:pet_type', (req, res) => {
   const { pet_type } = req.params;
+
+  const key = Object.keys(pets).find((key) => {
+    return key === pet_type;
+  });
+
+  // console.log(key);
+  // console.log(pets[key])
+  const animals = pets[key];
+  // console.log(animals);
+  let html = '';
+  html += '<ul>';
+  for (let i = 0; i < animals.length; i++) {
+    //   console.log(animals[i].name);
+    html += `<li>${animals[i].name}</li>`;
+  }
+  html += '</ul>';
+
+//   console.log(html);
   res.send(`
           <h1>List of ${pet_type}:</h1>
+          <ul>${html}</ul>
           `);
 });
 
