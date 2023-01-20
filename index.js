14f3770e638561316f7fac232977a99096bd3264
mariblan <99274158+mariblan@users.noreply.github.com>
2023-01-20 13:57:11 +0100

    step 3

--- index.js
+++ index.js
@@ -9,12 +9,19 @@ app.get('/', (req, res) => {
     <h1>Adopt Pet!</h1>
     <p>Browse through the links below to find your new furry friend:</p>
     <ul>
-        <li><a href="/dogs">Dogs</a></li>
-        <li><a href="/cats">Cats</a></li>
-        <li><a href="/cats">Rabbits</a></li>
+        <li><a href="/animals/dogs">Dogs</a></li>
+        <li><a href="/animals/cats">Cats</a></li>
+        <li><a href="/animals/cats">Rabbits</a></li>
     </ul>
     `);
 });
+
+app.get('/animals', (req, res) => {
+  res.send(`
+        <h1>List of pets:</h1>
+        `);
+});
+
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
